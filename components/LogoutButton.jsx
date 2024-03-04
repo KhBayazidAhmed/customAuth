78585bf0d69e665c520de3aa348b6f1a9951a7da
KhBayazidAhmed <khandakerbayazidahmed@gmail.com>
2024-03-04 20:41:12 +0600

    adding logout button loading screen

--- components/LogoutButton.jsx
+++ components/LogoutButton.jsx
@@ -1,22 +1,23 @@
 "use client";
 
 import { signOut } from "@/actions";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function LogoutButton() {
-  useEffect(() => {
-    console.log("run once");
-  });
+  const [loading, setLoading] = useState(false);
+  if (loading) return <p className="text-white">Loading...</p>;
   return (
     <button
       onClick={async () => {
+        setLoading(true);
         let res = await signOut();
         if (res.response) {
           window.location.reload();
         }
+        setLoading(false);
       }}
       href="logout"
-      className="bg-red-600 rounded p-2 text-white hover:text-gray-300"
+      className=" rounded p-2 text-white hover:text-gray-300"
     >
       Log out
     </button>
